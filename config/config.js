599a318137969b97de8e356839d90cf7c31d8c7d
Bobby Huang <zhenghuahuang@gmail.com>
2017-12-04 08:40:50 -0500

    Deployment: migration to google cloud platform

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "bobbyh",
+    "password": null,
+    "database": "jobby_dev",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "jobby_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": process.env.SQL_USER,
+    "password": process.env.SQL_PASSWORD,
+    "database": process.env.SQL_DATABASE,
+    "host": `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+    "dialect": "postgres"
+  }
+}
